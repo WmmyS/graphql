1e13f16007895ee7fc9e6f7196bd3e014e66d232
Wesley Santos <wesley@okn.com.br>
2023-04-09 20:14:59 -0300

    feat: Adição de start de projeto Vue.

---
+++ frontend/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="text-center">
+    <h1>Graphql Cadastros</h1>
+    <h6 class=""></h6>
+
+  </div>
+</template>
+
+<script>
+import "bootstrap/dist/css/bootstrap.css"
+import "font-awesome/css/font-awesome.css"
+
+export default {
+  name: 'App',
+  components: {}
+}
+</script>
+
+<style>
+
+</style>
