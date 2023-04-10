7d2e072af60e9d1231b3896578717d2471cdc0a6
Wesley Santos <wesley@okn.com.br>
2023-04-09 21:08:48 -0300

    feat: Definida página de front.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,21 +1,93 @@
 <template>
-  <div class="text-center">
-    <h1>Graphql Cadastros</h1>
-    <h6 class=""></h6>
-
+  <div>
+    <div id="slogan" class="text-center">
+      <h1>Gerador de Nomes</h1>
+      <br />
+      <h6 class="secondary-text">
+        Teste de implementação de um CRUD com Vue.js e Graphql
+      </h6>
+    </div>
+    <div id="main">
+      <div class="container">
+        <div class="row">
+          <div class="col-md">
+            <h5>Prefixos <span class="badge bg-primary">0</span></h5>
+            <div class="card">
+              <div class="card-body">
+                <ul class="list-group">
+                  <li class="list-group-item" v-for="prefixo in prefixos" :key="prefixo">
+                    {{ prefixo }}
+                  </li>
+                  <br />
+                  <input
+                    class="form-control"
+                    type="text"
+                    placeholder="Digite o Prefixo"
+                  />
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="col-md">
+            <h5>Sufixos <span class="badge bg-primary">0</span></h5>
+            <div class="card">
+              <div class="card-body">
+                <ul class="list-group">
+                  <li class="list-group-item" v-for="sufixo in sufixos" :key="sufixo">
+                    {{ sufixo }}
+                  </li>
+                  <br />
+                  <input class="form-control" type="text" placeholder="Digite o Sufixo" />
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <br />
+        <h5>Domínios <span class="badge bg-primary"> {{ domains.length }} </span></h5>
+        <div class="card">
+          <div class="card-body">
+            <ul class="list-group">
+              <li class="list-group-item" v-for="domain in domains" :key="domain">
+                {{ domain }}
+              </li>
+              <br />
+              <input class="form-control" type="text" placeholder="Digite o Sufixo" />
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import "bootstrap/dist/css/bootstrap.css"
-import "font-awesome/css/font-awesome.css"
+import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css";
 
 export default {
-  name: 'App',
-  components: {}
-}
+  name: "App",
+  data: function () {
+    return {
+      prefixos: ["Air", "Jet", "Flight"],
+      sufixos: ["Hub", "Station", "Mart"],
+      domains: ["AirHub", "JetStation", "FlightMart"]
+    };
+  },
+  components: {},
+};
 </script>
 
 <style>
+#slogan {
+  background-color: #3f51b5;
+  color: white;
+  padding: 20px;
+}
 
+#main {
+  background-color: #f1f1f1;
+  padding-top: 30px;
+  padding-bottom: 30px;
+}
 </style>
