ba021bcc2c53bc5f309bcb93bcc5b1337665ef76
WmmyS <wesleymarcosm@gmail.com>
2023-04-09 22:28:03 -0300

    feat: Adição de botões de adição.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -11,7 +11,7 @@
       <div class="container">
         <div class="row">
           <div class="col-md">
-            <h5>Prefixos <span class="badge bg-primary">0</span></h5>
+            <h5>Prefixos <span class="badge bg-primary"> {{ prefixos.length }}</span></h5>
             <div class="card">
               <div class="card-body">
                 <ul class="list-group">
@@ -19,17 +19,26 @@
                     {{ prefixo }}
                   </li>
                   <br />
-                  <input
-                    class="form-control"
-                    type="text"
-                    placeholder="Digite o Prefixo"
-                  />
+                  <div class="input-group">
+                    <input
+                      class="form-control"
+                      type="text"
+                      placeholder="Digite o Prefixo"
+                      v-model="newPrefixo"
+                      v-on:onkeyup.enter="addPrefixo"
+                    />
+                    <div class="input-group-append">
+                      <button class="btn btn-primary" v-on:click="addPrefixo">
+                        <span class="fa fa-plus"></span>
+                      </button>
+                    </div>
+                  </div>
                 </ul>
               </div>
             </div>
           </div>
           <div class="col-md">
-            <h5>Sufixos <span class="badge bg-primary">0</span></h5>
+            <h5>Sufixos <span class="badge bg-primary"> {{ sufixos.length }}</span></h5>
             <div class="card">
               <div class="card-body">
                 <ul class="list-group">
@@ -37,7 +46,20 @@
                     {{ sufixo }}
                   </li>
                   <br />
-                  <input class="form-control" type="text" placeholder="Digite o Sufixo" />
+                  <div class="input-group">
+                    <input
+                      class="form-control"
+                      type="text"
+                      placeholder="Digite o Sufixo"
+                      v-model="newSufixo"
+                      v-on:onkeyup.enter="addSufixo"
+                    />
+                    <div class="input-group-append">
+                      <button class="btn btn-primary" v-on:click="addSufixo">
+                        <span class="fa fa-plus"></span>
+                      </button>
+                    </div>
+                  </div>
                 </ul>
               </div>
             </div>
@@ -71,10 +93,26 @@ export default {
 	name: "App",
 	data: function () {
 		return {
+			newPrefixo: "",
+			newSufixo: "",
 			prefixos: ["Air", "Jet", "Flight"],
 			sufixos: ["Hub", "Station", "Mart"],
 			domains: ["AirHub", "JetStation", "FlightMart"],
 		};
+	},
+	methods: {
+		addPrefixo() {
+			if (this.newPrefixo !== "") {
+				this.prefixos.push(this.newPrefixo);
+				this.newPrefixo = "";
+			}
+		},
+		addSufixo() {
+			if (this.newSufixo !== "") {
+				this.sufixos.push(this.newSufixo);
+				this.newSufixo = "";
+			}
+		},
 	},
 	components: {},
 };
